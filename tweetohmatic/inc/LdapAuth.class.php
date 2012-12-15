98faba0a4907168997560f74981d0c53add96b94
Koen Martens <gmc@ohm2013.org>
2012-12-15 19:18:18 +0000

    - fix auth and add userlist

--- tweetohmatic/inc/LdapAuth.class.php
+++ tweetohmatic/inc/LdapAuth.class.php
@@ -3,24 +3,43 @@
 require_once(dirname(__FILE__)."/Auth.class.php");
 
 class LdapAuth extends Auth {
-	privat $conn;
+	private $conn;
 	
 	public function __construct() {
 		$this->conn = ldap_connect(LDAP_HOST);
 		if(!$this->conn)
 			throw new Exception("Unable to connect to ldap host.");
+		ldap_set_option($this->conn, LDAP_OPT_PROTOCOL_VERSION, 3);
 	}
 	
 	public function authenticate($user, $password) {
-		if(ldap_bind($this->conn, "cn=$user,".LDAP_ROOT, $password)) {
+		if(ldap_bind($this->conn, LDAP_ATTR."=$user,".LDAP_ROOT, $password)) {
 			$_SESSION['authenticated']=time();
-			$_SESSION['user']=$row['username'];
+			$_SESSION['user']=$user;
 			return true;
 		} else {
 			return false;
 		}			
 	}
 	
+	public function getUsers() {
+		$users = array();
+
+		ldap_bind($this->conn,LDAP_BIND_DN,LDAP_BIND_PW);
+	
+		$result = ldap_search(
+			$this->conn,
+			LDAP_ROOT,
+			LDAP_FILTER,
+			array(LDAP_ATTR)
+		);
+		if($result) {
+			$entries=ldap_get_entries($this->conn, $result);
+			for($i=0;$i<$entries['count'];$i++)
+				array_push($users,$entries[$i]['uid'][0]);
+		}
+		return $users;
+	}
 }
 
-?>
+
