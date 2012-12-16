020982d37aa31efc72b0c3c249abb2f3ddad1903
Koen Martens <gmc@sonologic.nl>
2012-12-16 15:40:23 +0100

    - include ldap settings

--- tweetohmatic/private/config.example.php
+++ tweetohmatic/private/config.example.php
@@ -7,9 +7,11 @@ $dbpath = 'private/db.sqlite';
 define('AUTH_METHOD', 'db');
 //define('AUTH_METHOD', 'ldap');
 
-define('LDAP_HOST', 'ldaps://localhost/');
-define('LDAP_ROOT', 'ou=users,dc=yoyodyne,dc=com');
-
-define('DEFAULT_PERM', array('queue'));
+//define('LDAP_HOST', 'ldaps://localhost/');
+//define('LDAP_ROOT', 'ou=users,dc=yoyodyne,dc=com');
+//define('LDAP_ATTR', 'uid');
+//define('LDAP_FILTER', '(objectClass=posixAccount)');
+//define("LDAP_BIND_DN", "uid=tweetohmatic,".LDAP_ROOT);
+//define("LDAP_BIND_PW", "secret");
 
 ?>
