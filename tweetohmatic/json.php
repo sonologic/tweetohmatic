ef9dff0b1e927be8076ad34df1a7e7800646d164
Koen Martens <gmc@sonologic.nl>
2012-12-16 15:45:12 +0100

    - add GPL boilerplate

--- tweetohmatic/json.php
+++ tweetohmatic/json.php
@@ -1,5 +1,25 @@
 <?php
 
+/**
+ *    This file is part of Tweet-ohm-matic.
+ *
+ *    Tweet-ohm-matic is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Tweet-ohm-matic is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with Tweet-ohm-matic.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author "Koen Martens" <gmc@sonologic.nl>
+ *
+ */
+
 require_once('private/config.php');
 require_once('inc/lib.php');
 require_once('inc/db.class.php');
